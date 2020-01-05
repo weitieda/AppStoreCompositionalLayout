d24f588be4df9b9a0820ef24bd0f11e1adcd68b1
Tieda Wei <weitieda@gmail.com>
2020-01-04 22:49:15 -0500

    Initial Commit

---
+++ AppStoreCompositionalLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreCompositionalLayout
+//
+//  Created by Tieda Wei on 2020-01-04.
+//  Copyright © 2020 Tieda Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
